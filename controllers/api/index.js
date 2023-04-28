bcb6c556e5f98c3e9bf931779ae45a0a03371102
zacharydserafin <zacharydserafin@gmail.com>
2023-04-27 20:23:23 -0500

    Made Folder Structure and Added Boilerplate Code

---
+++ controllers/api/index.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+const blogRoutes = require('./blogRoutes');
+const commentRoutes = require('./commentRoutes');
+const userRoutes = require('./userRoutes');
+
+// router.use('/blog', blogRoutes);
+// router.use('/comment', commentRoutes);
+// router.use('/users', userRoutes);
+
+
+module.exports = router;
